4e0dda2b53a5283836af7ab6063286a77838b825
Carsten Behring <carsten.behring@gmail.com>
2015-01-27 23:26:36 +0100

    added aggressive indent mode

--- init.el
+++ init.el
@@ -188,4 +188,22 @@
 
 (highlight-tail-reload)
 
+(global-undo-tree-mode)
+(global-set-key (kbd "C-z") 'undo-tree-visualize)
+
+(require 'tramp)
+
+(defun find-file-as-root ()
+  "Like `ido-find-file, but automatically edit the file with
+root-privileges (using tramp/sudo), if the file is not writable by
+user."
+  (interactive)
+  (let ((file (ido-read-file-name "Edit as root: ")))
+    (unless (file-writable-p file)
+      (setq file (concat "/sudo:root@localhost:" file)))
+    (find-file file)))
+
+(global-aggressive-indent-mode 1)
+
+
 (dired "~/Dropbox")
