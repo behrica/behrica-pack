a9a043a44e5e1a66b2b720b4b1d418d03e5c386b
Carsten Behring <carsten.behring@gmail.com>
2015-01-27 09:10:28 +0100

    - added copy-line - added tail-highlight

--- init.el
+++ init.el
@@ -102,7 +102,7 @@
 
 (server-start)
 
-(set-frame-parameter nil 'fullscreen 'fullboth)
+;;(set-frame-parameter nil 'fullscreen 'fullboth)
 
 
 (setq browse-url-browser-function 'eww-browse-url) ; use eww as default browser
@@ -136,6 +136,56 @@
   (quote
    ("e56f1b1c1daec5dbddc50abd00fcd00f6ce4079f4a7f66052cf16d96412a09a9" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default))))
 
+
+ (defun copy-line (arg)
+    "Copy lines (as many as prefix argument) in the kill ring.
+      Ease of use features:
+      - Move to start of next line.
+      - Appends the copy on sequential calls.
+      - Use newline as last char even on the last line of the buffer.
+      - If region is active, copy its lines."
+    (interactive "p")
+    (let ((beg (line-beginning-position))
+          (end (line-end-position arg)))
+      (when mark-active
+        (if (> (point) (mark))
+            (setq beg (save-excursion (goto-char (mark)) (line-beginning-position)))
+          (setq end (save-excursion (goto-char (mark)) (line-end-position)))))
+      (if (eq last-command 'copy-line)
+          (kill-append (buffer-substring beg end) (< end beg))
+        (kill-ring-save beg end)))
+    (kill-append "\n" nil)
+    (beginning-of-line (or (and arg (1+ arg)) 2))
+    (if (and arg (not (= 1 arg))) (message "%d lines copied" arg)))
+
+ (global-set-key "\C-c\C-k" 'copy-line)
+(defun duplicate-current-line (&optional n)
+      "duplicate current line, make more than 1 copy given a numeric argument"
+      (interactive "p")
+      (save-excursion
+        (let ((nb (or n 1))
+    	  (current-line (thing-at-point 'line)))
+          ;; when on last line, insert a newline first
+          (when (or (= 1 (forward-line 1)) (eq (point) (point-max)))
+    	(insert "\n"))
+
+          ;; now insert as many time as requested
+          (while (> n 0)
+    	(insert current-line)
+    	(decf n)))))
+
+(global-set-key (kbd "C-c C-l") 'duplicate-current-line)
+
 (sml/setup)
+
+
+(require 'highlight-tail)
+(setq highlight-tail-colors '(("black" . 0)
+                               ("#bc2525" . 25)
+                               ("black" . 100)))
+(setq highlight-tail-steps 10
+       highlight-tail-timer 0.05)
+
+(highlight-tail-reload)
 
 (dired "~/Dropbox")
