8db6a69eb5048a1ce49922f5c48e288308e3b297
Carsten Behring <carsten.behring@gmail.com>
2015-02-02 17:42:39 +0100

    added jedi and jedi-direx

--- init.el
+++ init.el
@@ -156,5 +156,11 @@ user."
                                         ; don't split windows
 (setq py-split-window-on-execute t)
 
+(eval-after-load "python"
+  '(define-key python-mode-map "\C-cx" 'jedi-direx:pop-to-buffer))
+(add-hook 'jedi-mode-hook 'jedi-direx:setup)
+
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
 
 (dired "~/Dropbox")
